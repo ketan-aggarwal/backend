d47bd14c56f6fb2f50b4bb6404a0e6581af754ea
ketan aggarwal <ketanaggarwal322@gmail.com>
2026-06-14 20:31:15 +0530

    Import java.util.List in AuthController to fix build compilation

--- src/main/java/com/example/demo/controller/AuthController.java
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -13,6 +13,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Collections;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
